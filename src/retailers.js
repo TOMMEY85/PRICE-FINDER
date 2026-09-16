a3dd5cda4a0a332c4a35304f6c9c381ae8e842b7
TOMMEY85 <r3d.tommey@gmail.com>
2026-09-16 12:06:12 +0200

    Ajoute la liste des revendeurs

---
+++ src/retailers.js
@@ -0,0 +1,13 @@
+export const retailers = [
+  {"id":"grosbill","name":"Grosbill","mode":"scraping"},
+  {"id":"materiel","name":"Materiel.net","mode":"scraping"},
+  {"id":"cybertek","name":"Cybertek","mode":"scraping"},
+  {"id":"ldlc","name":"LDLC.COM","mode":"scraping"},
+  {"id":"pccomponentes","name":"PcComponentes.fr","mode":"scraping"},
+  {"id":"rueducommerce","name":"Rue du Commerce","mode":"scraping"},
+  {"id":"infomax","name":"Infomax Paris","mode":"scraping"},
+  {"id":"memorypc","name":"Memory PC","mode":"scraping"},
+  {"id":"pcspecialist","name":"PCSpecialist.fr","mode":"scraping"},
+  {"id":"boulanger","name":"Boulanger","mode":"scraping"},
+  {"id":"maxesport","name":"Maxesport","mode":"scraping"}
+];
